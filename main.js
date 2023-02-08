0eb13734ab258a6020454a6f3f82cbb2574ae4d1
Aman Gupta <124691938+amantpcg01@users.noreply.github.com>
2023-02-08 15:20:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,36 +12,6 @@ const MongoHelper = require("./src/helpers/mongo.helper");
 const { MongoClient } = require('mongodb');
 const { APP_PORT, HOST, NODE_ENV, DB_HOST, DB_PORT, DBNAME } = process.env;
 
-
-// async function main() {
-//   const uri = `mongodb://${DB_HOST}:${DB_PORT}/${DBNAME}`;
-//   const client = new MongoClient(uri);
-
-//   try {
-//     await client.connect();
-//     console.log("Connected");
-//   } catch (e) {
-//     console.error(e);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// main();
-
-// const server = http.createServer(app);
-// const port = 3000;
-// const host = HOST || "localhost";
-// const env = NODE_ENV || "development";
-// try {
-//   server.listen(port, () => {
-//     console.log(`Server running at http://${host}:${port} in ${env} mode`);
-//   });
-// } catch (error) {
-//   console.log(error);
-// }
-
-
 // create http server
 const runServer = async () => {
   const server = http.createServer(app);
